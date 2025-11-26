d77009f9a417524cf224bc7d41c50d961b409c5b
Kevin Willocks <kevinwillocks@gmail.com>
2025-11-25 21:49:44 -0600

    Updated auth file for accessing the state file remotely on S3

--- 0-auth.tf
+++ 0-auth.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-tuesday-1125"
+    key    = "112525/terraformTuesday/terraform.tfstate" //File path and name
+    region = "us-west-1"
+  }
+}
+
+
 provider "aws" {
     region = "us-west-1"
 }
